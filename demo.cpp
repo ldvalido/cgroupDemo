c4c51b8f5408ab0669d42928930c3643a11e25fd
Leandro Valido <leandrovalido@gmail.com>
2020-07-26 00:16:21 -0300

    Initial commit

---
+++ demo.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <new>
+#include <cstdlib>
+int main()
+{
+    int i = 0;
+    char *ptr = NULL;
+    while (i < 50)
+    {
+        if ((ptr = (char *)malloc(1048576)) == NULL)
+        { ///1MB allocated
+            std::cout << "Allocation fails at " << i << "MB\n";
+            return 0;
+        }
+        std::cout << "Allocated " << i + 1 << "MB\n";
+        i++;
+    }
+    std::cout << "Finished allocation";
+    return 0;
+}
